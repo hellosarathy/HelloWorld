7631f26a5dbcff3768a52a9632072cd96a0ea709
hellosarathy <45619186+hellosarathy@users.noreply.github.com>
2018-12-05 15:12:48 +0530

    Update HelloTest.java

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -19,8 +19,8 @@ public class HelloTest {
 	WebElement text;
 	@BeforeTest
 	public void beforeTest() {
-		/*	System.setProperty("webdriver.gecko.driver", "resources\\geckodriver32.exe");
-		driver = new FirefoxDriver();*/
+	System.setProperty("webdriver.gecko.driver", "resources\\geckodriver32.exe");
+		driver = new FirefoxDriver();
 		System.setProperty("webdriver.chrome.driver","resources\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("http://localhost:8080/Helloworld.war/");
